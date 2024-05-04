f3c71738efaa1d35528032064aba01e24ed21559
jayveer-singh-sisodiya <jayveersisodiya66@gmail.com>
2024-05-04 10:10:26 +0530

    first

--- myreact/src/components/Navbar.js
+++ myreact/src/components/Navbar.js
@@ -30,7 +30,7 @@ export default function Navbar(props) {
       }}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" herf="/">
+        <a className="navbar-brand" herf="#">
           {props.title}
         </a>
         <button
@@ -47,7 +47,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link " aria-current="page" herf="/">
+              <a className="nav-link " aria-current="page" herf="#">
                 Home
               </a>
             </li>
